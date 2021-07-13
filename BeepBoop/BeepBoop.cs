1f86f6746210876ad04362d7ffeca6b7e64e87d5
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-13 09:19:58 -0700

    Alister update

---
+++ BeepBoop/BeepBoop.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BeepBoop.Calculating
+{
+  class BeepBooper
+  {
+   
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 using System; //give us access to the Console object and its methods
+using System.Collections.Generic;
+using BeepBoop.Calculating;
 
 
-class Program 
+namespace BeepBoop
 {
-  static void Main() //automatically run, an entry point method
+  class Program 
   {
+    static void Main() //automatically run, an entry point method
+    {
+      Console.WriteLine("enter a number: ");
+      string stringNumber = Console.ReadLine();
+      int number = int.Parse(stringNumber);
 
+      int[] numberArray = new int[number];  
 
+      for(int i = 0; i < number; i++)
+      {
+        numberArray[i] = i;
+      }
+      Console.WriteLine(numberArray);
+      
+
+    }
   }
 }
